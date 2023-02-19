e82c6eecd55271225024be26c50326feb087de8d
coglebradley <cogle@mail.bradley.edu>
2023-02-18 22:27:11 -0600

    Assignment 4 Done

--- EscapeRoom/Source/EscapeRoom/MyPlayerController.h
+++ EscapeRoom/Source/EscapeRoom/MyPlayerController.h
@@ -13,10 +13,10 @@ UCLASS()
 class ESCAPEROOM_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OurVariables")
 
-		float health = 0.5f;
-	
+	float Health = 0.5f;
 };
