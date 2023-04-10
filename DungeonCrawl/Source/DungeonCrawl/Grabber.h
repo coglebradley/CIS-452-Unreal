6a96e2b01fb19f159e3d7e5139378be1257d5627
coglebradley <cogle@mail.bradley.edu>
2023-04-09 23:53:33 -0500

    Finish Assignment9

---
+++ DungeonCrawl/Source/DungeonCrawl/Grabber.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DUNGEONCRAWL_API UGrabber : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+public:
+	UFUNCTION(BlueprintCallable)
+		void Grab();
+	UFUNCTION(BlueprintCallable)
+		void Release();
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+		float MaxGrabDistance = 400;
+
+	UPROPERTY(EditAnywhere)
+		float GrabRadius = 100;
+	UPROPERTY(EditAnywhere)
+		float HoldDistance = 200;
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
